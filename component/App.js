b0624919b4d9811681a9aa2a1505b1b3c9bfdc61
ruba-alhomaid <52470078+ruba-alhomaid@users.noreply.github.com>
2019-10-24 19:44:34 +0300

    create actions, reducers, middleware and the store.

---
+++ component/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import { handleInitialData } from '../actions/shared'
+
+class App extends Component {
+    componentDidMount() {
+        this.props.dispatch(handleInitialData())
+    }
+    render() {
+        return(
+            <div>
+                Hello World
+            </div>
+        )
+    }
+}
+
+export default connect()(App)
---
+++ index.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import ReactDom from 'react-dom'
+import App from './component/App'
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
+import reducer from './reducers'
+import middleware from './middleware'
+
+const store = createStore(reducer, middleware)
+
+ReactDom.render(
+    <Provider store={store}>
+        <App />
+    </Provider>,
+    document.getElementById('root'))
